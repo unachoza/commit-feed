27f2d7ab42fab9b7f5c8a080d076b3106d898715
arianna choza <unachoza@gmail.com>
2022-01-08 18:13:41 -0500

    fetching possible repositories from github repository search

--- src/API/githubFetch.tsx
+++ src/API/githubFetch.tsx
@@ -1,16 +1,26 @@
-const GITHUB_BASE_URL = 'https://api.github.com';
-const SORT_BY = 'stars';
-const ORDER = 'desc';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export const repositoryFetch = (
-  queryText: string,
-  queryLicense: string,
-  queryFork: boolean,
-  queryStars: string
-): Promise<any> => {
-  return fetch(
-    `${GITHUB_BASE_URL}/search/repositories?q=${encodeURIComponent(queryText)}+license:${encodeURIComponent(
-      queryLicense
-    )}+stars:${encodeURIComponent(queryStars)}+fork:${queryFork}&sort=${SORT_BY}&order=${ORDER}`
-  );
+const GITHUB_BASE_URL = 'https://api.github.com';
+
+export const repositoryFetch = async( queryText: string): Promise<any> => {
+  const data = await axios.get(`${GITHUB_BASE_URL}/search/repositories?q=${encodeURIComponent(queryText)}`);
+  const items = data.data.items
+  const repositoryChoices = shapeData(items)
+  console.log(repositoryChoices)
+  return repositoryChoices
 };
+
+ //Make fetched data easier to work with
+ const shapeData = (input: any) => {
+   let shapedData = [];
+   for (let el of input) {
+    shapedData.push(normalizeRepoData(el));
+  }
+  return shapedData;
+};
+
+const normalizeRepoData = (data: any) => ({
+  repoName: data.name, 
+  repoOwner: data.owner.login 
+})
