1e874739064f56696868773c58a3090c2eafe8a7
arianna choza <unachoza@gmail.com>
2022-01-07 20:34:54 -0500

    added loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+export interface LoadingState {
+  onLoading: boolean;
+  onError: boolean;
+}
+
+const INITIALSTATE: LoadingState = { onLoading: false, onError: false };
+const loadingStatus: { LoadingState: any } | any = {};
 
 
-const App: React.FC = ()=> {
+const App: React.FC = () => {
+  const [loadingState, setLoadingState] = useState<LoadingState>(INITIALSTATE);
   return (
     <div className="App">
       <header className="App-header">
