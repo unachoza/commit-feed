54ac2e5361fa8e8f859d633450f30fd7280fe408
arianna choza <unachoza@gmail.com>
2022-01-12 18:19:08 -0500

    added typescript to textInput

--- src/Components/Input/TextInput.tsx
+++ src/Components/Input/TextInput.tsx
@@ -1,7 +1,11 @@
-//@ts-nocheck
 import React from "react";
 
-const TextInput = ({ placeholder, setSearchValue }) => {
+interface TextInputProps{ 
+  placeholder: string;
+  setSearchValue: (value: string) => void;
+}
+const TextInput = (props: TextInputProps): JSX.Element => {
+  const { placeholder, setSearchValue } = props
   return <div className="text-input"><input type="text" placeholder={placeholder} onBlur={(e) => setSearchValue(e.target.value)}/></div>;
 };
 export default TextInput;
